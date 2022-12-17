c12e777b8bbf69a3b129efded62f280784055bc6
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-12-16 18:49:40 -0800

    fixed include line

---
+++ publish.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mosquitto.h>
+
+int main(){
+
+    //variables
+	int connect;
+	struct mosquitto * mqtt;
+
+
+	mosquitto_lib_init();
+	mqtt = mosquitto_new("project", true, NULL);
+	connect = mosquitto_connect(mqtt, "localhost", 1883, 60);
+
+
+    //checks to see if it is connected or not
+	if(connect != 0){
+
+		printf("Client could not connect to broker! Error Code: %d\n", connect);
+		mosquitto_destroy(mqtt);
+		return -1;
+    
+	}
+
+    //connected to the broker
+	printf("We are now connected to the broker!\n");
+
+	mosquitto_publish(mqtt, NULL, "project", 6, "Hello", 0, false);
+	mosquitto_disconnect(mqtt);
+	mosquitto_destroy(mqtt);
+
+	mosquitto_lib_cleanup();
+	return 0;
+}
