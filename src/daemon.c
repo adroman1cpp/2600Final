3e6bc9823871ad4d464263050bab9a5a5afba334
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-12-16 18:16:25 -0800

    daemon that runs in the background

---
+++ src/daemon.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+
+int main()
+{
+    
+    signal(SIGINT, SIG_IGN);
+    signal(SIGTERM, SIG_IGN);
+
+    reciever();
+
+    return 0;
+}
+
+
+void reciever()
+{
+    while (1)
+    {
+        
+        char* game = getenv("GAME");
+
+        //if that runs the game 
+        if (game != NULL) {
+            // Starts the game
+            system("./tictactoe");
+
+            setenv("GAME", "", 1); //resets the variable
+        } else { //else that keeps on checking
+            
+            sleep(20);
+        }
+    }
+}
